1ae918df970694e45f94b125602bbadf7670ff1b
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-07-10 13:22:32 +0200

    Changing formatting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { ApolloServer } from "apollo-server";
-import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
-import { schema } from "./schema";
+import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { schema } from './schema';
 
 export const server = new ApolloServer({
-    schema,
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-})
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 const port = 3001;
 
-server.listen({port}).then(({url})=> {console.log(`Server is running on ${url}`)});
+server.listen({ port }).then(({ url }) => {
+  console.log(`Server is running on ${url}`);
+});
